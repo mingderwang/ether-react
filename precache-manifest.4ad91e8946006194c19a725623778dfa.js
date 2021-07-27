6bf64a7ef473813b9f0796fc17a78744d16d39fb
Ming-der Wang <mingderwang@gmail.com>
2021-07-27 21:51:49 +0800

    Updates

---
+++ precache-manifest.4ad91e8946006194c19a725623778dfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cb1e020a3118e4cca655e63d95e75d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e9ed1fbe47abae615c5",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "deac9784d8f0eb284473",
+    "url": "./static/js/2.a3fafb56.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a3fafb56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9ed1fbe47abae615c5",
+    "url": "./static/js/main.5f35ac5a.chunk.js"
+  },
+  {
+    "revision": "14648121500e6570037c",
+    "url": "./static/js/runtime-main.c14f0692.js"
+  }
+]);
