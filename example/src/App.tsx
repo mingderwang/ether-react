e6197622383e38a9652d36642008bb85f7a70b20
Ming-der Wang <mingderwang@gmail.com>
2021-07-28 16:36:58 +0800

    rc0.0.1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import { ExampleComponent } from 'ether-react'
+import { ExampleComponent, today } from 'ether-react'
 import 'ether-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="CC 😄" />
+  (async function run() {
+    await today()
+  })()
+  console.log(ExampleComponent)
+  return <ExampleComponent text="CC- 😄" />
 }
 
 export default App
---
+++ src/ExampleComponent.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import { useState, useEffect } from "react";
+import styles from './styles.module.css'
+import { Sdk, MetaMaskWalletProvider } from 'etherspot'
+import { ethers } from 'ethers'
+import { today, tomorrow } from './utils'
+
+interface Props {
+  text: string
+}
+declare let window: any;
+
+export const ExampleComponent = ({ text }: Props) => {
+  let sdk: Sdk
+  const [val, setVal] = useState(0)
+  const [chainId] = useState(-1)
+  const [account, setAccount] = useState('👻');
+
+  useEffect(() => {
+    if (!MetaMaskWalletProvider.detect()) {
+      alert('Please install metamask first.')
+    } else {
+      (async function run() {
+        if (window) {
+          const walletProvider = await MetaMaskWalletProvider.connect();
+          const provider = new ethers.providers.Web3Provider(window.ethereum)
+          const signer = provider.getSigner()
+          console.log(signer)
+          const addr = await signer.getAddress()
+          console.log(addr)
+          console.log('AAAAxxx.signer ', signer)
+          sdk = new Sdk(walletProvider)
+          const output = await sdk.syncAccount();
+          console.log('account', output);
+          await today()
+          await tomorrow()
+          setAccount(output.address)
+
+        }
+      })();
+    }
+  }, []);
+
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      setVal((v: number) => v + 1);
+    }, 1000);
+    return () => window.clearInterval(timer);
+  }, []);
+
+  return <div className={styles.test}>
+    👑 Ethereum: {text} {val} {account} chainId: 0x{chainId}
+  </div>
+}
